809aaeb426282a66b5f4ce09bb12e77fdd4e5551
Chantal Wallace <theteatreehouse@gmail.com>
2018-06-09 01:38:53 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,17 @@
 def unsafe?(speed)
-
+  
+  if
+    speed < 40 || speed > 60
+    return true 
+  end
+  
+  else 
+    return false 
+    
 end
 
-
-
 def not_safe?(speed)
-	
+	speed < 40 || speed > 60 ? true : false 
 end
 	
 
